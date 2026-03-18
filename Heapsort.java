a107bbbc81f8b8314fb69e0f5edcb3b27130c23c
MofThabang <4362668@myuwc.ac.za>
2026-03-18 14:56:41 +0200

    Update Heapsort.java
    buildheapbottomup class is added to form heap from array

--- Heapsort.java
+++ Heapsort.java
@@ -1,1 +1,9 @@
+Public Static Void buildHeapBottomUp(String[]arr){
+  heap =arr.clone();
+  size = heap.length
 
+    // start from last non-leaf node
+    for(i=size/2-1;i>=0;i--;){
+    HeapfyDown(i);
+  }
+}
